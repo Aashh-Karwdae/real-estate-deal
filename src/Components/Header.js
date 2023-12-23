209666b4b37ac4fd7a274cfc679cb06f1b925dfe
Ashwin Karwade <ashwinkarwade54321@gmail.com>
2023-12-23 15:44:52 +0530

    create Header the fisrt Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import { IoSearch } from "react-icons/io5";
+import {Link} from "react-router-dom";
+
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='fheader'>
+     <Link className="Header" to='/Home'><h1 className='HdrHeading'>Aashh<span className='secheading'>EstateDeal</span></h1></Link>
+     <form>
+    <div className='serachbar'>
+      <input type='text' placeholder='Search...' className='SearchInput'/>
+       <IoSearch className='SearcIcon'/>
+    </div>
+    </form>
+    <div className='Pages'>
+      
+      <Link to="/Home" className='page'><span className='page'>Home</span></Link>
+      <Link to="/About" className='page'><span className='page'>About</span></Link>
+      <Link  to="/SignIn" className='page'><span className='page'>Sign in</span></Link>
+      
+    
+      
+    
+    </div>
+    </div>
   )
 }
