aa3865503593bc99275f11878d75dfe8931160e2
Ashwin Karwade <ashwinkarwade54321@gmail.com>
2023-12-23 15:37:34 +0530

    Install React Router Dom and create Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Header from './Components/Header';
+import About from './Pages/About ';
+import Home from './Pages/Home';
+import SignIn from './Pages/Sign In';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Aashh whatsp!!</h1>
+      <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/Home' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/SignIn' element={<SignIn/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
